9deabe1df16739a5bf2d9bff5a9437997402ca28
Leon <leon@mobvista.com>
2019-01-21 18:28:53 +0800

    add pprof

---
+++ cmd/pprof/pprof_runtime.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"github.com/lzxbill7/mygo/pprof"
+	"math/rand"
+)
+
+func doSomething1() {
+	i := 0
+	for {
+		i++
+	}
+}
+
+func doSomething2() {
+	i := 1
+	for {
+		i *= 2
+	}
+}
+
+func doSomething3() {
+	for {
+		doSomething4()
+	}
+}
+
+func doSomething4() {
+	rand.Int()
+}
+
+func main() {
+	go doSomething1()
+	go doSomething2()
+	go doSomething3()
+
+	if err := pprof.PPCmd("cpu 10s"); err != nil {
+		panic(err)
+	}
+
+	if err := pprof.PPCmd("mem"); err != nil {
+		panic(err)
+	}
+}
---
+++ pprof/pprof.go
@@ -0,0 +1,76 @@
+package pprof
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"runtime/pprof"
+	"strings"
+	"time"
+)
+
+func PPMem(suffix string) error {
+	fp, err := os.Create(fmt.Sprintf("mem.pprof.%v", suffix))
+	if err != nil {
+		return errors.New(fmt.Sprintf("PPMem failed. suffix: [%v], err: [%v]", suffix, err))
+	}
+	defer fp.Close()
+
+	if err := pprof.WriteHeapProfile(fp); err != nil {
+		return errors.New(fmt.Sprintf("PPMem failed. suffix: [%v], err: [%v]", suffix, err))
+	}
+
+	return nil
+}
+
+func PPCpu(suffix string, duration time.Duration) error {
+	fp, err := os.Create(fmt.Sprintf("cpu.pprof.%v", suffix))
+	if err != nil {
+		return errors.New(fmt.Sprintf("PPCpu failed. suffix: [%v], duration: [%v], err: [%v]", suffix, duration, err))
+	}
+	defer fp.Close()
+
+	if err := pprof.StartCPUProfile(fp); err != nil {
+		return errors.New(fmt.Sprintf("PPCpu failed. suffix: [%v], duration: [%v], err: [%v]", suffix, duration, err))
+	}
+	defer pprof.StopCPUProfile()
+
+	time.Sleep(duration)
+
+	return nil
+}
+
+// "mem [suffix=yyyymmddhh]" 生成内存 pprof 文件
+// "cpu [duration=60s] [suffix=yyyymmddhh]" 生成 cpu pprof 文件
+func PPCmd(command string) error {
+	fields := strings.Split(command, " ")
+	cmdType := fields[0]
+
+	switch cmdType {
+	case "mem":
+		suffix := time.Now().Format("200601021504")
+		if len(fields) >= 2 {
+			suffix = fields[1]
+		}
+
+		return PPMem(suffix)
+	case "cpu":
+		duration := 60 * time.Second
+		if len(fields) >= 2 {
+			var err error
+			duration, err = time.ParseDuration(fields[1])
+			if err != nil {
+				return errors.New(fmt.Sprintf("PPCmd failed. command: [%v], err: [%v]", command, err))
+			}
+		}
+
+		suffix := time.Now().Format("200601021504")
+		if len(fields) >= 3 {
+			suffix = fields[2]
+		}
+
+		return PPCpu(suffix, duration)
+	default:
+		return errors.New(fmt.Sprintf("PPCmd failed. invalid command: [%v]", command))
+	}
+}
