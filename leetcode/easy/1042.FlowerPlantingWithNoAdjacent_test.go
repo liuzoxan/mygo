01f52c472ee9b89662c75337dcbe0d378071880e
Liuzoxan <liuzoxan@gmail.com>
2019-08-20 14:46:48 +0800

    update

--- leetcode/easy/1042.FlowerPlantingWithNoAdjacent_test.go
+++ leetcode/easy/1042.FlowerPlantingWithNoAdjacent_test.go
@@ -6,48 +6,29 @@ import (
 )
 
 func gardenNoAdj(N int, paths [][]int) []int {
-	plant := make(map[int]int)
-	nextPaths := make(map[int][][]int)
+	nextPaths := make([][]int, N)
 	for _, p := range paths {
-		if _, OK := nextPaths[p[0]]; !OK {
-			nextPaths[p[0]] = [][]int{}
-		}
-		nextPaths[p[0]] = append(nextPaths[p[0]], p)
-
-		if _, OK := nextPaths[p[1]]; !OK {
-			nextPaths[p[1]] = [][]int{}
-		}
-		nextPaths[p[1]] = append(nextPaths[p[1]], p)
+		i, j := p[0]-1, p[1]-1
+		nextPaths[i] = append(nextPaths[i], j)
+		nextPaths[j] = append(nextPaths[j], i)
 	}
 
-	for i := 1; i <= N; i++ {
+	plant := make([]int, N)
+	for i := 0; i < N; i++ {
+		isUsed := [5]bool{}
+		for _, p := range nextPaths[i] {
+			isUsed[plant[p]] = true
+		}
+
 		for j := 1; j <= 4; j++ {
-			same := false
-			for _, p := range nextPaths[i] {
-				if p[0] == i {
-					if plant[p[1]] == j {
-						same = true
-						break
-					}
-				} else if p[1] == i {
-					if plant[p[0]] == j {
-						same = true
-						break
-					}
-				}
-			}
-			if !same {
+			if !isUsed[j] {
 				plant[i] = j
 				break
 			}
 		}
 	}
 
-	var res []int
-	for i := 1; i <= N; i++ {
-		res = append(res, plant[i])
-	}
-	return res
+	return plant
 }
 
 func TestGardenNoAdj(t *testing.T) {
