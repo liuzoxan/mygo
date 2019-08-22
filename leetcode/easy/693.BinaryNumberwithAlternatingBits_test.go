b5cf69bce568842c4a5eade49dfa4016ae6add31
Liuzoxan <liuzoxan@gmail.com>
2019-08-22 14:31:06 +0800

    update

--- leetcode/easy/693.BinaryNumberwithAlternatingBits_test.go
+++ leetcode/easy/693.BinaryNumberwithAlternatingBits_test.go
@@ -22,13 +22,10 @@ func hasAlternatingBits1(n int) bool {
 }
 
 func hasAlternatingBits(n int) bool {
-	pre := -1
-
 	for n > 0 {
-		if pre == n&1 {
+		if n&1 == (n>>1)&1 {
 			return false
 		}
-		pre = n & 1
 		n >>= 1
 	}
 
