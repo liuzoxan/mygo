6ab9d796864757be3376d492784a01c7211cdcea
Liuzoxan <liuzoxan@gmail.com>
2019-08-27 22:05:31 +0800

    update

--- leetcode/medium/673.NumberofLongestIncreasingSubsequence_test.go
+++ leetcode/medium/673.NumberofLongestIncreasingSubsequence_test.go
@@ -6,36 +6,49 @@ import (
 )
 
 func findNumberOfLIS(nums []int) int {
-	if len(nums) == 1 {
-		return 1
-	} else if len(nums) == 0 {
-		return 0
+	size := len(nums)
+
+	length := make([]int, size)
+	count := make([]int, size)
+	for i := 0; i < size; i++ {
+		count[i] = 1
 	}
 
-	count := 1
-	max := 1
-	for i := 1; i < len(nums); i++ {
-		if nums[i] >= nums[i-1] {
-			count++
-		} else {
-			if count > max {
-				max = count
+	for j, n := range nums {
+		for i := 0; i < j; i++ {
+			if nums[i] < n {
+				if length[i] >= length[j] {
+					length[j] = 1 + length[i]
+					count[j] = count[i]
+				} else if length[i]+1 == length[j] {
+					count[j] = count[j] + count[i]
+				}
 			}
-			count = 0
 		}
 	}
 
-	if count > max {
-		max = count
+	var maxLen int
+	for _, l := range length {
+		if l > maxLen {
+			maxLen = l
+		}
 	}
 
-	return max
+	var sum int
+	for i, c := range count {
+		if length[i] == maxLen {
+			sum += c
+		}
+	}
+
+	return sum
 }
 
 func TestFindNumberOfLIS(t *testing.T) {
 	Convey("test detect capital", t, func() {
 		So(findNumberOfLIS([]int{1, 3, 5, 4, 7}), ShouldEqual, 2)
 		So(findNumberOfLIS([]int{2, 2, 2, 2, 2}), ShouldEqual, 5)
+		So(findNumberOfLIS([]int{1, 2, 4, 3, 5, 4, 7, 2}), ShouldEqual, 3)
 	})
 }
 
