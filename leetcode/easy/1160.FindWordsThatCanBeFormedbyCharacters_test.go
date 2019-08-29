43bf2fe8271926b750dd9ae1b7539eac1fbda82d
Liuzoxan <liuzoxan@gmail.com>
2019-08-29 21:32:43 +0800

    update

--- leetcode/easy/1160.FindWordsThatCanBeFormedbyCharacters_test.go
+++ leetcode/easy/1160.FindWordsThatCanBeFormedbyCharacters_test.go
@@ -1,1 +1,45 @@
 package easy
+
+import (
+	. "github.com/smartystreets/goconvey/convey"
+	"testing"
+)
+
+func countCharacters(words []string, chars string) int {
+	tableS := make(map[rune]int, 26)
+	for _, r := range chars {
+		tableS[r] ++
+	}
+
+	// dynamic table for loop
+	table := make(map[rune]int, 26)
+
+	var res string
+	for _, str := range words {
+		// fresh table
+		for r, c := range tableS {
+			table[r] = c
+		}
+
+		match := true
+		for _, r := range str {
+			table[r]--
+			if table[r] < 0 {
+				match = false
+				break
+			}
+		}
+		if match {
+			res += str
+		}
+	}
+
+	return len(res)
+}
+
+func TestCountCharacters(t *testing.T) {
+	Convey("test", t, func() {
+		So(countCharacters([]string{"cat", "bt", "hat", "tree"}, "atach"), ShouldEqual, 6)
+		So(countCharacters([]string{"hello","world","leetcode"}, "welldonehoneyr"), ShouldEqual, 10)
+	})
+}
