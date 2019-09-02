8566853daf648d6ea6e639e40b69d8aa2d127c2e
Liuzoxan <liuzoxan@gmail.com>
2019-09-02 21:03:27 +0800

    update

---
+++ leetcode/easy/1029.TwoCityScheduling_test.go
@@ -0,0 +1,73 @@
+package easy
+
+import (
+	. "github.com/smartystreets/goconvey/convey"
+	"math"
+	"sort"
+	"testing"
+)
+
+type Cost struct {
+	A    int
+	B    int
+	diff float64
+}
+
+type Costs []Cost
+
+func (c Costs) Len() int {
+	return len(c)
+}
+
+func (c Costs) Less(i, j int) bool {
+	return c[i].diff > c[j].diff
+}
+
+func (c Costs) Swap(i, j int) {
+	c[i], c[j] = c[j], c[i]
+}
+
+func twoCitySchedCost(costs [][]int) int {
+	var nCosts Costs
+	for _, c := range costs {
+		nCosts = append(nCosts, Cost{
+			A:    c[0],
+			B:    c[1],
+			diff: math.Abs(float64(c[0]) - float64(c[1])),
+		})
+	}
+	sort.Sort(nCosts)
+	// log.Println(nCosts)
+
+	countA := len(costs) / 2
+	countB := countA
+	minCost := 0
+	for _, c := range nCosts {
+		if countA == 0 {
+			minCost += c.B
+			continue
+		}
+
+		if countB == 0 {
+			minCost += c.A
+			continue
+		}
+
+		if c.A < c.B {
+			countA--
+			minCost += c.A
+		} else {
+			countB--
+			minCost += c.B
+		}
+	}
+
+	return minCost
+}
+
+func TestTwoCitySchedCost(t *testing.T) {
+	Convey("test", t, func() {
+		So(twoCitySchedCost([][]int{{10, 20}, {30, 200}, {400, 50}, {30, 20}}), ShouldEqual, 110)
+		So(twoCitySchedCost([][]int{{259, 770}, {448, 54}, {926, 667}, {184, 139}, {840, 118}, {577, 469}}), ShouldEqual, 1859)
+	})
+}
