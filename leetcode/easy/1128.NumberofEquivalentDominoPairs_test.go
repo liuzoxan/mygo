ddb7f4a841bb2af6d6877e993c332494184e9315
Liuzoxan <liuzoxan@gmail.com>
2019-09-03 20:36:07 +0800

    update

---
+++ leetcode/easy/1128.NumberofEquivalentDominoPairs_test.go
@@ -0,0 +1,31 @@
+package easy
+
+import (
+	. "github.com/smartystreets/goconvey/convey"
+	"testing"
+)
+
+func numEquivDominoPairs(dominoes [][]int) int {
+	m := make(map[int]int, 100)
+
+	for _, d := range dominoes {
+		if d[0] <= d[1] {
+			m[d[0]*10+d[1]] ++
+		} else {
+			m[d[1]*10+d[0]] ++
+		}
+	}
+
+	sum := 0
+	for _, c := range m {
+		sum += (c - 1) * c / 2
+	}
+
+	return sum
+}
+
+func TestNumEquivDominoPairs(t *testing.T) {
+	Convey("test", t, func() {
+		So(numEquivDominoPairs([][]int{{1, 2}, {2, 1}, {3, 4}, {5, 6}}), ShouldEqual, 1)
+	})
+}
