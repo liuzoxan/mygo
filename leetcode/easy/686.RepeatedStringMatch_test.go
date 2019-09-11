a88e061eec48720f3e90c75999d3614dafb4762a
Liuzoxan <liuzoxan@gmail.com>
2019-09-11 19:56:01 +0800

    update

---
+++ leetcode/easy/686.RepeatedStringMatch_test.go
@@ -0,0 +1,30 @@
+package easy
+
+import (
+	. "github.com/smartystreets/goconvey/convey"
+	"strings"
+	"testing"
+)
+
+func repeatedStringMatch(A string, B string) int {
+	la := len(A)
+	lb := len(B)
+	max := lb/la + 2
+	str := ""
+	n := 1
+	for n <= max {
+		str += A
+		if strings.Contains(str, B) {
+			return n
+		}
+		n++
+	}
+
+	return -1
+}
+
+func TestRepeatedStringMatch(t *testing.T) {
+	Convey("test", t, func() {
+		So(repeatedStringMatch("abcd", "cdabcdab"), ShouldEqual, 3)
+	})
+}
