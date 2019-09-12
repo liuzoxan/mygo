e3c71d682b669b63d7979834fb1154307d56fa8e
Liuzoxan <liuzoxan@gmail.com>
2019-09-12 18:56:39 +0800

    update

---
+++ leetcode/easy/832.FlippinganImage_test.go
@@ -0,0 +1,27 @@
+package easy
+
+import (
+	. "github.com/smartystreets/goconvey/convey"
+	"testing"
+)
+
+func flipAndInvertImage(A [][]int) [][]int {
+	for _, s := range A {
+		for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+			s[i], s[j] = s[j], s[i]
+		}
+
+		for i := 0; i < len(s); i++ {
+			s[i] = s[i] ^ 1
+		}
+	}
+
+	return A
+}
+
+func TestFlipAndInvertImage(t *testing.T) {
+	Convey("test", t, func() {
+		t.Log(flipAndInvertImage([][]int{{1, 1, 0}, {1, 0, 1}, {0, 0, 0}}))
+		t.Log(flipAndInvertImage([][]int{{1, 1, 0, 0}, {1, 0, 0, 1}, {0, 1, 1, 1}, {1, 0, 1, 0}}))
+	})
+}
