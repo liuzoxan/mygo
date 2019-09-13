60ea6a517579f5bfebaa0470dbea6bab6ab59b0c
Liuzoxan <liuzoxan@gmail.com>
2019-09-13 08:44:50 +0800

    update

--- leetcode/easy/832.FlippinganImage_test.go
+++ leetcode/easy/832.FlippinganImage_test.go
@@ -5,10 +5,20 @@ import (
 	"testing"
 )
 
-func flipAndInvertImage(A [][]int) [][]int {
+func flipAndInvertImage2(A [][]int) [][]int {
 	for _, s := range A {
 		for i, j := 0, len(s)-1; i <= j; i, j = i+1, j-1 {
 			s[i], s[j] = s[j]^1, s[i]^1
+		}
+	}
+
+	return A
+}
+
+func flipAndInvertImage(A [][]int) [][]int {
+	for _, s := range A {
+		for i := 0; i < (len(A)+1)/2; i++ {
+			s[i], s[len(A)-i-1] = s[len(A)-i-1]^1, s[i]^1
 		}
 	}
 
