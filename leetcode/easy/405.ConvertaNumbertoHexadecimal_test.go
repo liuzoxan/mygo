f0687b4da2607dd16adfa7b323880103b3252606
Liuzoxan <liuzoxan@gmail.com>
2019-09-15 15:23:42 +0800

    update

---
+++ leetcode/easy/405.ConvertaNumbertoHexadecimal_test.go
@@ -0,0 +1,15 @@
+package easy
+
+import (
+	. "github.com/smartystreets/goconvey/convey"
+	"testing"
+)
+
+func toHex(num int) string {
+	return ""
+}
+
+func TestToHex(t *testing.T) {
+	Convey("test detect capital", t, func() {
+	})
+}
