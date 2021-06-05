8f793c9d1f1c903df0c6c6ec976e771c56690854
Liuzoxan <liuzoxan@gmail.com>
2021-06-05 18:17:24 +0800

    update

--- test/json_test.go
+++ test/json_test.go
@@ -8,7 +8,19 @@ import (
 func TestJson(t *testing.T) {
 	jstr := []byte(`{"1":{"a":1, "b":2}, "2":{"a":3, "b":4}}`)
 	var data map[string]interface{} // map[1] = interface{} map[2]=interface{}
- 	json.Unmarshal(jstr, &data)
+	json.Unmarshal(jstr, &data)
 
 	t.Log(data)
+}
+
+func TestJson_Print(t *testing.T) {
+	type Person struct {
+		Name string `json:"object"`
+		Age  int
+	}
+
+	p := Person{}
+
+	json.Unmarshal([]byte(`{"object":"tom","Age":10}`), &p)
+	t.Log(p)
 }
